55d3bee82f591dfb88c6d9baf8f7576349326305
longtime1116 <longtime1116@gmail.com>
2018-07-24 09:14:54 +0900

    first implement done

--- cs50/pset5/speller/dictionary.c
+++ cs50/pset5/speller/dictionary.c
@@ -2,14 +2,59 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "dictionary.h"
+
+
+char **table;
+int word_num;
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    (void)word;
+    int from = 0;
+    int to = word_num - 1;
+    char lower_word[strlen(word) + 1];
+
+    strcpy(lower_word, word);
+    for(int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (word[i] >= 'A' && word[i] <= 'Z')
+        {
+            lower_word[i] -= 'A' - 'a';
+        }
+    }
+
+    while(1)
+    {
+        if (from > to)
+        {
+            break;
+        }
+
+        int index = (from + to) / 2;
+        int result = strcmp(lower_word, table[index]);
+
+        if (result > 0)
+        {
+            from = index + 1;
+            continue;
+        }
+        else if (result < 0)
+        {
+            to = index - 1;
+            continue;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
     return false;
 }
 
@@ -24,28 +69,44 @@ bool load(const char *dictionary)
     }
 
     char word[LENGTH + 1];
+    int count = 0;
 
     while(fgets(word, LENGTH + 1, file) != NULL)
     {
+        count++;
+    }
+
+    table = malloc(sizeof(char *) * count);
+    word_num = count;
+
+    rewind(file);
+    int i = 0;
+    while(fgets(word, LENGTH + 1, file) != NULL)
+    {
         word[strlen(word) - 1] = '\0';
-        printf("word: %s\n", word);
+
+        char *str = malloc(strlen(word) + 1);
+        strcpy(str, word);
+        table[i] = str;
+        i++;
     }
     fclose(file);
 
-
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return word_num;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < word_num; i++)
+    {
+        free(table[i]);
+    }
+    return true;
 }
