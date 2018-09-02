8126bcb4246892ea21c7ed7ec4dcc6a64506b0dd
longtime1116 <longtime1116@gmail.com>
2018-09-02 18:59:44 +0900

    loop

---
+++ introduction/mitcs/loop.py
@@ -0,0 +1,16 @@
+print("while1")
+n = ""
+while n != "yes":
+    n = input("type yes :")
+print("you typed 'yes'")
+
+
+print("while2")
+n = 0
+while(n < 5):
+    print(n)
+    n += 1
+
+print("for")
+for i in range(5,9):
+    print(i) #=> 5 6 7 8
