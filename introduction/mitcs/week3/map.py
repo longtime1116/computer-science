e7360f343427159055b4be9e1f9eb24b874a7869
shumo.tokinaga <longtime1116@gmail.com>
2018-09-18 16:56:05 +0900

    update map sample

--- introduction/mitcs/week3/map.py
+++ introduction/mitcs/week3/map.py
@@ -3,3 +3,21 @@ for e in map(abs, [-1, 4, -5, 6]):
 
 for e in map(min, [-1, 4, -5, 6], [-2, 5, -4, 7]):
     print(e)
+
+
+
+def applyToEach(L, f):
+    for i in range(len(L)):
+        L[i] = f(L[i])
+
+testList = [1, -4, 8, -9]
+applyToEach(testList, abs)
+print(testList)
+
+testList = [1, -4, 8, -9]
+applyToEach(testList, lambda x: x + 1)
+print(testList)
+
+testList = [1, -4, 8, -9]
+applyToEach(testList, lambda x: x * x)
+print(testList)
