cbee3fabcd40d7fa6397be70410e3e8df59e0b03
longtime1116 <longtime1116@gmail.com>
2018-10-08 11:57:01 +0900

    add getter/setter

--- introduction/mitcs/week5/coordinate.py
+++ introduction/mitcs/week5/coordinate.py
@@ -2,6 +2,19 @@ class Coordinate(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
     def distance(self, other):
         x_diff_sq = (self.x - other.x) ** 2
         y_diff_sq = (self.y - other.y) ** 2
@@ -12,5 +25,8 @@ c = Coordinate(3, 4)
 origin = Coordinate(0, 0)
 
 print(c.distance(origin))
+print(Coordinate.distance(c, origin))
+
 
 # from coordinate import Coordinate
+
