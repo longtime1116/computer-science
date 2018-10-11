5da18e6eb039f52054f7500409e83abdd7f322e5
shumo.tokinaga <longtime1116@gmail.com>
2018-10-11 14:47:19 +0900

    pset6, p2

--- introduction/mitcs/week5/ps6/ps6.py
+++ introduction/mitcs/week5/ps6/ps6.py
@@ -158,7 +158,10 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = dict(self.build_shift_dict(shift))
+        self.message_text_encrypted = self.apply_shift(shift)
 
     def get_shift(self):
         '''
@@ -166,7 +169,7 @@ class PlaintextMessage(Message):
 
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -174,7 +177,7 @@ class PlaintextMessage(Message):
 
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        return dict(self.encrypting_dict)
 
     def get_message_text_encrypted(self):
         '''
@@ -182,7 +185,7 @@ class PlaintextMessage(Message):
 
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -195,7 +198,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        self.shift = shift
+        self.encrypting_dict = dict(self.build_shift_dict(shift))
+        self.message_text_encrypted = self.apply_shift(shift)
 
 
 class CiphertextMessage(Message):
