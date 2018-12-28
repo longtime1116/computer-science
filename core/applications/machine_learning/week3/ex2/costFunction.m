1a14a37a8056988a69d4653995f3cb88cbbfe2c2
shumo.tokinaga <longtime1116@gmail.com>
2018-12-28 20:14:55 +0900

    途中 costFunction.m

--- core/applications/machine_learning/week3/ex2/costFunction.m
+++ core/applications/machine_learning/week3/ex2/costFunction.m
@@ -20,7 +20,24 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+X(4, :);
+theta
+-y(4)
+1-y(4)
+theta' * sigmoid(X(4, :))'
+log(theta' * sigmoid(X(4, :))')
+1 - theta' * sigmoid(X(4, :))'
+- y(4) * log(theta' * sigmoid(X(4, :))') - (1 - y(4)) * log(1 - theta' * sigmoid(X(4, :))')
 
+for i=1:m
+  J = J - y(i) * log(sigmoid(theta' * X(i, :))') - (1 - y(i)) * log(1 - sigmoid(theta' * X(i, :)));
+end
+J = J / m
+
+%for i=1:m
+%  J = J + ([X(i, :)] * theta - y(i))^2;
+%end
+%J = J / (2 * m);
 
 
 
