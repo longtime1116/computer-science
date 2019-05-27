319816bedfe2ae0110598670c10b2d722d25c594
shumo.tokinaga <longtime1116@gmail.com>
2019-05-27 17:04:04 +0900

    mult.asm

--- core/systems/nand2tetris/projects/04/mult/mult.asm
+++ core/systems/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// R10: i(0, 1, ..., R2-1)
+(LOOP)
+  // if (@i == R2) goto END
+  @R10
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JEQ
+
+  // R2にR0を足す
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  // i++
+  @R10
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+
